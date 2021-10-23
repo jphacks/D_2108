cb7840ad703bbd26a59b3ecaabc169fb49068c20
itoka229mori <itoka229mori@gmail.com>
2021-10-24 02:10:49 +0900

    #7 入室画面に最低限必要な入力部分を用意

---
+++ pages/login-room.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>部屋名</p>
+    <div>
+      <p>名前</p>
+      <input>
+    </div>
+    <button>オフライン参加</button>
+    <button>オンライン参加</button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
