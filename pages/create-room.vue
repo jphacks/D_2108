aca5a64381b29168803370486e58f1133512184e
itoka229mori <itoka229mori@gmail.com>
2021-10-24 01:54:51 +0900

    #1 部屋作成画面に最低限必要な入力部分を用意

---
+++ pages/create-room.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+   <div class="nameinput">
+      <p>なまえ</p>
+      <input>
+    </div>
+    <div class="roominput">
+      <p>部屋名入力欄</p>
+      <input>
+    </div>
+    <div class="createbutton">
+      <button>作成</button>
+    </div>   
+  </div>
+
+</template>
+
+<script>
+export default {}
+</script>
