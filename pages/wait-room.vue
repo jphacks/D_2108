3ab698f0d0a60f0601948a54b2c4e6cf1820f2d5
itoka229mori <itoka229mori@gmail.com>
2021-10-24 16:55:32 +0900

    #10 部屋に入ったユーザーの表示をcomponentに切り分け

--- pages/wait-room.vue
+++ pages/wait-room.vue
@@ -2,25 +2,13 @@
   <div>
     <p>オフライン</p>
     <div class="flex space-x-4 ...">
-      <p>{{url}}</p>
+      <p>{{ url }}</p>
       <button @click="copyUrlToClipboard(url)">コピー</button>
       <button>QRコード</button>
     </div>
     <div>
-      <div class="flex space-x-4 ...">
-        <div class="icon">
-          <p>オフ</p>
-          <img src="" alt="">
-        </div>
-        <p>なまえ</p>
-      </div>
-      <div class="flex space-x-4 ...">
-        <div class="icon">
-          <p>オン</p>
-          <img src="" alt="">
-        </div>
-        <p>なまえ</p>
-      </div>      
+      <off-user-block></off-user-block>
+      <on-user-block></on-user-block>
     </div>
     <button>終了</button>
     <button>更新</button>
@@ -28,18 +16,20 @@
 </template>
 
 <script>
+import OffUserBlock from '~/components/OffUserBlock.vue'
+import OnUserBlock from '~/components/OnUserBlock.vue'
+
 export default {
-  data(){
-    return{
-      url:"http://localhost:3000/wait-room"
+  components: { OffUserBlock, OnUserBlock },
+  data() {
+    return {
+      url: 'http://localhost:3000/wait-room',
     }
   },
-  methods:{
-    copyUrlToClipboard(text){
-      navigator.clipboard.writeText(text).catch((e)=>{
-
-      });
-    }
-  }
+  methods: {
+    copyUrlToClipboard(text) {
+      navigator.clipboard.writeText(text).catch((e) => {})
+    },
+  },
 }
 </script>
