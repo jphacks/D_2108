fa0ae1b7ad44285ab95a4c3fd5322a4ca120b282
itoka229mori <itoka229mori@gmail.com>
2021-10-24 11:42:34 +0900

    #10 待機画面に最低限必要な入力部分を用意

---
+++ pages/wait-room.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>オフライン</p>
+    <div class="flex space-x-4 ...">
+      <p>url</p>
+      <button>コピー</button>
+      <button>QRコード</button>
+    </div>
+    <div>
+      <div class="flex space-x-4 ...">
+        <div class="icon">
+          <p>オフ</p>
+          <img src="" alt="">
+        </div>
+        <p>なまえ</p>
+      </div>
+      <div class="flex space-x-4 ...">
+        <div class="icon">
+          <p>オン</p>
+          <img src="" alt="">
+        </div>
+        <p>なまえ</p>
+      </div>      
+    </div>
+    <button>終了</button>
+    <button>更新</button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
