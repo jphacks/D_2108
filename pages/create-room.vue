effc9d6d3355059c987864469c0bd7e48e2305d0
itoka229mori <itoka229mori@gmail.com>
2021-10-27 01:50:33 +0900

    #1 変数入力欄に変数を設定

--- pages/create-room.vue
+++ pages/create-room.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
-   <div class="nameinput">
+    <div class="nameinput">
       <p>なまえ</p>
-      <input>
+      <input v-model="name" placeholder="おなまえ" />
     </div>
     <div class="roominput">
       <p>部屋名入力欄</p>
-      <input>
+      <input v-model="roomName" placeholder="" />
     </div>
     <div class="createbutton">
       <button>作成</button>
-    </div>   
+    </div>
   </div>
-
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      name: '',
+      roomName: '',
+    }
+  },
+}
 </script>
