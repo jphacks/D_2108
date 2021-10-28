ac32bba3a8d2c037964a6aa2f2392ef1e77048bb
watson70percent <gozaru5364@gmail.com>
2021-10-28 19:37:32 +0900

    #21 envファイルをいじれるように

--- front/pages/videoChat.vue
+++ front/pages/videoChat.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>
-      Video Call<br><small style="font-size: 14pt;">Powered by Agora.io</small>
+      Video Call<br /><small style="font-size: 14pt">Powered by Agora.io</small>
     </h1>
     <h4>Local video</h4>
     <div id="me"></div>
@@ -16,69 +16,73 @@ if (process.browser) {
 }
 export default {
   name: 'Helloworld',
-  mounted({$config}) {
+  mounted() {
     const client = AgoraRTC.createClient({
-      mode: "rtc",
-      codec: "vp8",
-    });
-    console.log("AgoraRTC = "+AgoraRTC);
+      mode: 'rtc',
+      codec: 'vp8',
+    })
     const localStream = AgoraRTC.createStream({
       audio: true,
       video: true,
-    });
-    console.log('====init====');
-    client.init($config.appID);
-    console.log('====join====')
-    client.join($config.token, 0, null, ()=>{
-      localStream.init(()=>{
-      localStream.play("me");
-      client.publish(localStream, handleError);
-      }, handleError);
-      client.on("stream-added", function(evt){
-        client.subscribe(evt.stream, handleError);
-      });
-      client.on("stream-subscribed", function(evt){
-        const stream = evt.stream;
-        const streamId = String(stream.getId());
-        addVideoStream(streamId);
-        stream.play(streamId);
-      });
-      client.on("stream-removed", function(evt){
-        const stream = evt.stream;
-        const streamId = String(stream.getId());
-        stream.close();
-        removeVideoStream(streamId);
-      });
-      client.on("peer-leave", function(evt){
-        const stream = evt.stream;
-        const streamId = String(stream.getId());
-        stream.close();
-        removeVideoStream(streamId);
-      });
-    }, handleError);
-  }
+    })
+    client.init(process.env.appID)
+    client.join(
+      process.env.token,
+      0,
+      null,
+      () => {
+        localStream.init(() => {
+          localStream.play('me')
+          client.publish(localStream, handleError)
+        }, handleError)
+        client.on('stream-added', function (evt) {
+          client.subscribe(evt.stream, handleError)
+        })
+        client.on('stream-subscribed', function (evt) {
+          const stream = evt.stream
+          const streamId = String(stream.getId())
+          addVideoStream(streamId)
+          stream.play(streamId)
+        })
+        client.on('stream-removed', function (evt) {
+          const stream = evt.stream
+          const streamId = String(stream.getId())
+          stream.close()
+          removeVideoStream(streamId)
+        })
+        client.on('peer-leave', function (evt) {
+          const stream = evt.stream
+          const streamId = String(stream.getId())
+          stream.close()
+          removeVideoStream(streamId)
+        })
+      },
+      handleError
+    )
+  },
 }
-const handleError = function(err){
-  console.log("Error: ", err);
-};
-function addVideoStream(elementId){
-  const streamDiv = document.createElement("div");
-  streamDiv.id = elementId;
-  streamDiv.style.transform = "rotateY(180deg)";
-  streamDiv.style.width = "400px";
-  streamDiv.style.height = "400px";
-  document.getElementById("container").appendChild(streamDiv);
+const handleError = function (err) {
+  console.log('Error: ', err)
+}
+function addVideoStream(elementId) {
+  const streamDiv = document.createElement('div')
+  streamDiv.id = elementId
+  streamDiv.style.transform = 'rotateY(180deg)'
+  streamDiv.style.width = '400px'
+  streamDiv.style.height = '400px'
+  document.getElementById('container').appendChild(streamDiv)
 }
 function removeVideoStream(elementId) {
-  const remoteDiv = document.getElementById(elementId);
-  if (remoteDiv) remoteDiv.parentNode.removeChild(remoteDiv);
+  const remoteDiv = document.getElementById(elementId)
+  if (remoteDiv) remoteDiv.parentNode.removeChild(remoteDiv)
 }
 </script>
 <style scoped>
 * {
   font-family: sans-serif;
 }
-h1,h4 {
+h1,
+h4 {
   text-align: center;
 }
 #me {
