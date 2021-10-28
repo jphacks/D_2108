1628902c419d76f4ba566104176d69c34c8c00c3
itoka229mori <itoka229mori@gmail.com>
2021-10-29 03:46:53 +0900

    #11 部屋にいる人の一覧を取得する関数を追加

--- front/components/OffUserBlock.vue
+++ front/components/OffUserBlock.vue
@@ -3,7 +3,6 @@
     <div class="flex space-x-4 ...">
       <div class="icon">
         <p>オフ</p>
-        <img src="" alt="" />
       </div>
       <p>{{ name }}</p>
       <button>憑依</button>
@@ -13,10 +12,6 @@
 
 <script>
 export default {
-  data() {
-    return {
-      name: 'なまえ',
-    }
-  },
+  props: ['name'],
 }
 </script>
